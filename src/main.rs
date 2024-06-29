4180bacf81ad9b1ecc4a0432a80b7d9684c05186
NeroMorto <ragemorto@gmail.com>
2024-06-29 22:58:44 +0300

    sixth stage done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::FromStr;
+
+enum CommandInfo {
+    Echo,
+    Exit,
+    Type,
+}
+
+impl CommandInfo {
+    pub fn describe_command(command: CommandInfo) -> String {
+        match command {
+            CommandInfo::Echo => "echo is a shell builtin".to_string(),
+            CommandInfo::Exit => "exit is a shell builtin".to_string(),
+            CommandInfo::Type => "type is a shell builtin".to_string(),
+        }
+    }
+}
+
+impl FromStr for CommandInfo {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "echo" => Ok(CommandInfo::Echo),
+            "exit" => Ok(CommandInfo::Exit),
+            "type" => Ok(CommandInfo::Type),
+            nonexistent => Err(format!("{}: not found", nonexistent))
+        }
+    }
+}
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -15,25 +45,34 @@ fn main() {
 
 
     loop {
-
         stdin.read_line(&mut input).unwrap();
         match input.len() {
-            0 => {},
+            0 => {}
             _ => {
                 let command_with_possible_args = input.trim();
                 let (command, command_args) = command_with_possible_args.trim().split_once(" ").unwrap_or((command_with_possible_args, ""));
-                match command {
-                    "echo" => stdout.write_all(format!("{command_args}\r\n").as_bytes()).unwrap(),
-                    "exit" => if command_args == "0" {
-                        break;
-                    },
+                match CommandInfo::from_str(command) {
+                    Ok(command) => {
+                        match command {
+                            CommandInfo::Echo => stdout.write_all(format!("{command_args}\r\n").as_bytes()).unwrap(),
+                            CommandInfo::Exit => if command_args == "0" {
+                                break;
+                            }
+                            CommandInfo::Type => {
+                                match CommandInfo::from_str(command_args) {
+                                    Ok(c) => stdout.write_all(format!("{info}\r\n", info = CommandInfo::describe_command(c)).as_bytes()).unwrap(),
+                                    Err(err) => stdout.write_all(format!("{err}\r\n").as_bytes()).unwrap()
+                                };
+                            }
+                        }
+                    }
+
                     _ => stdout.write_all(format!("{command}: command not found\r\n").as_bytes()).unwrap()
                 }
                 input.clear();
                 print!("$ ");
                 io::stdout().flush().unwrap();
             }
-
         }
     }
 }
